59fe39c7c66e51d853bc67f926b515a87d79194c
Petr <mail@efimov-it.info>
2020-04-28 12:52:03 +0300

    Created base markup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-wrapper">
+    <img src="./assets/logo.svg" class="logo"/>
+    <h1>Vue-table</h1>
+    <p class="description">A Vue js component for easy table creation</p>
+    <VueTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueTable from './vue-table';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {VueTable}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0px;
+  padding: 0px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: #2a2f2a;
+  box-sizing: border-box;
+}
+body {
+  background-color: #f5f5f5;
+}
+.main-wrapper{
+  background-color: #ffffff;
+  max-width: 1024px;
+  min-height: 100vh;
+  margin: 0px auto;
+  padding: 0px 20px;
+}
+.logo {
+  display: block;
+  width: 25%;
+  margin: 0px auto;
+  padding-top: 30px;
+}
+h1 {
+  padding: 20px 0px 10px 0px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.description {
+  text-align: center;
+  color: #555555;
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/vue-table.vue
@@ -0,0 +1,39 @@
+<template>
+    <table>
+        <tr>
+            <th>123</th>
+            <th>123</th>
+        </tr>
+        <tr>
+            <td>Lorem ipsum</td>
+            <td>Ipsum lorem</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+table {
+    width: 100%;
+    border-radius: 3px;
+    border-collapse: collapse;
+    border: 1px solid #dedede;;
+
+    th {
+        text-align: left;
+    }
+
+    th, td {
+        padding: 10px;
+    }
+
+    th, tr:not(:last-child) {
+        border-bottom: 1px solid #dedede;
+    }
+}
+</style>
